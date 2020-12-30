155f1351c4448432227452cda5eeac89ea40e788
邓蔚竹 <waytrue@dengweizhus-MacBook-Pro.local>
2020-12-30 17:05:53 +0800

    update emacs mac

--- ZSH/install.sh
+++ ZSH/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/zsh
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
