a64b1b99c073d0108d496fd96f75ab68c5a0f79a
Your Name <you@example.com>
2023-02-01 10:59:53 +0800

    ...

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,16 +1,16 @@
 (require 'org)
 (org-babel-load-file
- (expand-file-name "~/.emacs.d/init-emacs.org"))
+ (expand-file-name (file-truename "~/.emacs.d/init-emacs.org")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default-text-scale-amount 30)
  '(org-roam-capture-templates
    '(("d" "default" plain "%?" :target
-      (file+head "${slug}.org" "#+title: ${title}
-")
+      (file+head "${slug}.org" "#+title: ${title}\12")
       :unnarrowed t)))
  '(org-roam-completion-everywhere t)
  '(package-selected-packages '(## use-package)))
